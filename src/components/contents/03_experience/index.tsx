455cf0b6fe76994142a74eb9b01d97a540112932
Yuichi-Kadota <yuichi_kadota@mediado.jp>
2022-03-27 00:38:51 +0900

    add experience

--- src/components/contents/03_experience/index.tsx
+++ src/components/contents/03_experience/index.tsx
@@ -1,10 +1,92 @@
 import AppBar from '../common/appbar';
-import Grid from '@mui/material/Grid';
+import { Grid } from '@mui/material';
+import Project from './project';
 
 export default function Experience() {
   return (
-    <>
-      <AppBar title={'Experience'}></AppBar>
-    </>
+    <Grid container columns={12}>
+      <Grid item xs={12}>
+        <AppBar title={'Experience'}></AppBar>
+      </Grid>
+      <Grid container direction="row" justifyContent="center">
+        <Project
+          src={'/contents/experience/project.png'}
+          title={'電子書籍流通システム　リライト'}
+          position={'Full-Stack Software Developer'}
+          doneList={[
+            '要件調整',
+            'フロントエンド実装',
+            'バックエンドAPI実装',
+            'インフラアーキテクチャ設計',
+            'インフラ構築',
+            '監視設計、構築',
+            'バッチ設計、構築',
+          ]}
+          techList={['Next.js', 'Go', 'AWS', 'Terraform']}
+          company="MEDIA DO Co., Ltd."
+          siteLink="https://mediado.jp/"
+          from="2021/05"
+        />
+        <Project
+          src={'/contents/experience/project.png'}
+          title={'電子書籍流通システム　AWS移行'}
+          position={'Backend Software Developer'}
+          doneList={[
+            '移行課題検討',
+            '認証基盤/API構築',
+            'インフラアーキテクチャ設計',
+            'インフラ構築',
+            '移行用スクリプト構築',
+            'PHP バージョンアップデート(4.4 > 7.3)',
+          ]}
+          techList={['Go', 'PHP', 'AWS', 'Terraform']}
+          company="MEDIA DO Co., Ltd."
+          siteLink="https://mediado.jp/"
+          from="2020/06"
+          to="2021/05"
+        />
+        <Project
+          src={'/contents/experience/engineer.png'}
+          title={'社内横断PoC案件'}
+          position={'Full-Stack Software Developer'}
+          doneList={[
+            '社内プライベートクラウド運用/保守',
+            'APIコンテナ基盤PoC',
+            'ブロックチェーンアプリPoC',
+            '業界ニュースレコメンドアプリPoC',
+          ]}
+          techList={['Vue.js', 'Node.js', 'Azure', 'k8s', 'istio', 'solidity']}
+          company="Mizuho Research & Technologies, Ltd."
+          siteLink="https://www.mizuho-ir.co.jp/index.html"
+          from="2019/06"
+          to="2020/05"
+        />
+        <Project
+          src={'/contents/experience/engineer.png'}
+          title={'決済基盤CIパイプライン構築'}
+          position={'Dev Ops Developer'}
+          doneList={[
+            'E2Eシナリオ検討、実行基盤',
+            'E2Eテスト実行パイプライン設計、構築',
+          ]}
+          techList={['Jenkins', 'Serenium', 'RHEL', 'Windows Server']}
+          company="Mizuho Research & Technologies, Ltd."
+          siteLink="https://www.mizuho-ir.co.jp/index.html"
+          from="2018/09"
+          to="2019/05"
+        />
+        <Project
+          src={'/contents/experience/engineer.png'}
+          title={'勘定系システム移行'}
+          position={'System Integrator'}
+          doneList={['要件調整', '進捗管理', 'テスト']}
+          techList={['COBOL', 'z/os', 'JCL']}
+          company="Mizuho Research & Technologies, Ltd."
+          siteLink="https://www.mizuho-ir.co.jp/index.html"
+          from="2017/04"
+          to="2018/08"
+        />
+      </Grid>
+    </Grid>
   );
 }
---
+++ src/components/contents/03_experience/project.tsx
@@ -0,0 +1,105 @@
+import Image from 'next/image';
+import {
+  Card,
+  CardContent,
+  Typography,
+  Toolbar,
+  List,
+  ListItemText,
+  Chip,
+} from '@mui/material';
+import { css } from '@emotion/react';
+
+interface Props {
+  src: string;
+  title: string;
+  position: string;
+  doneList: Array<string>;
+  techList: Array<string>;
+  company: string;
+  siteLink?: string;
+  from: string;
+  to?: string;
+}
+
+export default function Project(props: Props) {
+  return (
+    <Card sx={{ mt: 4, mb: 3, pb: 0, minWidth: (theme) => theme.spacing(90) }}>
+      <CardContent
+        // MuiCardContent-root-Projectのpadding-bottomを打ち消し
+        css={css`
+          &:last-child {
+            padding-bottom: 0px;
+          }
+        `}
+        sx={{ maxWidth: (theme) => theme.spacing(100), p: 0, pb: 0 }}
+      >
+        <Toolbar>
+          <Image src={props.src} width={28} height={28}></Image>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              ml: 1,
+              color: (theme) => theme.palette.text.secondary,
+              borderBottom: '1px solid',
+              borderColor: (theme) => theme.palette.grey[300],
+            }}
+          >
+            {props.title}
+          </Typography>
+        </Toolbar>
+        <Typography
+          variant="h6"
+          sx={{ ml: 3, color: (theme) => theme.palette.secondary.dark }}
+        >
+          {props.position}
+        </Typography>
+        <List sx={{ ml: 3, pb: 0 }}>
+          {props.doneList.map((v) => {
+            return (
+              <ListItemText
+                primaryTypographyProps={{
+                  variant: 'body1',
+                }}
+              >
+                ・{v}
+              </ListItemText>
+            );
+          })}
+        </List>
+        <Toolbar>
+          {props.techList.map((v) => {
+            return (
+              <Chip
+                label={v}
+                sx={{
+                  color: 'white',
+                  background: (theme) => theme.palette.secondary.main,
+                  ml: 1,
+                }}
+              />
+            );
+          })}
+        </Toolbar>
+        <Typography
+          sx={{ m: 1, ml: 3, mb: 2, color: (theme) => theme.palette.grey[600] }}
+          variant="body2"
+        >
+          {props.from}-{props.to}
+          {' _'}
+          <a
+            target="_blank"
+            css={(theme) => css`
+              text-decoration: none;
+              color: ${theme.palette.grey[700]};
+            `}
+            href={props.siteLink}
+          >
+            {props.company}
+          </a>
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
